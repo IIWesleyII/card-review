2c68a71bd0e02a811a71b1a7bdc4862c63e34bbd
leaf-turner <wesley.shaw.475@my.csun.edu>
2022-03-08 14:30:35 -0800

    added function to store all transactions made from coinbase card into database

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-# https://developers.coinbase.com/api/v2
+# https://docs.cloud.coinbase.com/sign-in-with-coinbase/docs/api-transactions
 # https://developers.coinbase.com/docs/wallet/api-key-authentication
 # https://medium.com/@samhagin/check-your-balance-on-coinbase-using-python-5641ff769f91
-
+import json
+from coinbase.wallet.client import Client
 import hmac, hashlib, time, requests, os, codecs
 from requests.auth import AuthBase
 from dotenv import load_dotenv
+import db
 
 load_dotenv()
 API_KEY = os.environ.get("API_KEY")
 API_SECRET = os.environ.get("API_SECRET")
+USDC_ACCOUNT_ID = os.environ.get("USDC_ACCOUNT_ID")
 
-# Create custom authentication for Coinbase API
 class CoinbaseWalletAuth(AuthBase):
     def __init__(self, api_key, secret_key):
         self.api_key = api_key
@@ -25,7 +27,7 @@ class CoinbaseWalletAuth(AuthBase):
             'CB-ACCESS-SIGN': signature,
             'CB-ACCESS-TIMESTAMP': timestamp,
             'CB-ACCESS-KEY': self.api_key,
-            'CB-VERSION': '2022-03-08'
+            'CB-VERSION': '2019-08-20'
         })
         return request
 
@@ -33,11 +35,14 @@ api_url = "https://api.coinbase.com/v2/"
 auth = CoinbaseWalletAuth(API_KEY, API_SECRET)
 
 
+def create_client():
+    client = Client(API_KEY, API_SECRET)
+    return client
+
 # GET current user
 def get_user(api_url,auth):
     r = requests.get(api_url + "user", auth=auth)
     return r.json()
-
 
 # https://api.coinbase.com/v2/accounts
 def get_accounts(api_url,auth):
@@ -48,3 +53,36 @@ def get_accounts(api_url,auth):
 def get_auth_info(api_url,auth):
     r = requests.get(f'{api_url}user/auth', auth=auth)
     return r.json()
+
+
+def save_accounts(client):
+    with open('coinbase_accounts.json','w') as f:
+        json.dump(client.get_accounts(limit=300),f)
+
+
+def store_transactions_in_db(conn):
+    coinbase_id = ''
+    transaction_type = ''
+    transaction_value = 0.0
+    transaction_date = ''
+    client = create_client()
+    transactions = client.get_transactions(USDC_ACCOUNT_ID,limit=300)
+
+    coinbase_id_set = set()
+    coinbase_ids = db.get_coinbase_ids(conn)
+    for id in coinbase_ids:
+        coinbase_id_set.add(id[0])
+    
+    for transaction in transactions['data']:
+        if transaction['id'] not in coinbase_id_set:
+            coinbase_id = transaction['id']
+            transaction_type = transaction['type']
+            transaction_value = float(transaction['amount']['amount'])
+            transaction_date = transaction['updated_at']
+            db.add_transaction(conn, coinbase_id, transaction_type,transaction_value,transaction_date)
+
+
+if __name__ == '__main__':
+    conn = db.connect()
+    db.create_table(conn)
+    store_transactions_in_db(conn)
---
+++ db.py
@@ -0,0 +1,53 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+CREATE_TRANSACTION_TABLE = '''
+CREATE TABLE IF NOT EXISTS transaction
+(id SERIAL PRIMARY KEY, coinbase_id TEXT UNIQUE, transaction_type TEXT, transaction_value DECIMAL, transaction_date TEXT);
+'''
+INSERT_TRANSACTION = '''
+INSERT INTO transaction (coinbase_id,transaction_type,transaction_value,transaction_date) 
+VALUES (%s,%s,%s,%s); 
+'''
+SELECT_COINBASE_ID = '''
+SELECT coinbase_id FROM transaction;
+'''
+
+
+def connect():
+    try:
+        conn = psycopg2.connect(
+            user ='postgres',
+            password=os.getenv('DB_PASSWORD'),
+            host='localhost',
+            database='coinbase-tracker'
+        )
+    except ConnectionError as exc:
+        raise RuntimeError('Failed to open database') from exc
+
+    return conn
+
+
+def create_table(conn): 
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(CREATE_TRANSACTION_TABLE)
+        cursor.close()
+
+
+def add_transaction(conn,coinbase_id,transaction_type,transaction_value,transaction_date):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(INSERT_TRANSACTION, (coinbase_id,transaction_type,transaction_value,transaction_date))
+        cursor.close()
+
+
+def get_coinbase_ids(conn):
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(SELECT_COINBASE_ID)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
